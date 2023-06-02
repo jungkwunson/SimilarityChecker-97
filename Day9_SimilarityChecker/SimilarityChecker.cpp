07435e020325d4c5862abc845f2a811180614173
JK <jksdasom@gmail.com>
2023-06-02 14:18:03 +0900

    코드 타입을 수정하였습니다.

--- Day9_SimilarityChecker/SimilarityChecker.cpp
+++ Day9_SimilarityChecker/SimilarityChecker.cpp
@@ -6,46 +6,44 @@ class Checker
 {
 
 public:
-	void getMinMax(string in1, string in2, float& max, float& min)
+	unsigned long long getMin(string in1, string in2)
 	{
 		if (in1.size() > in2.size())
 		{
-			max = (float)in1.size();
-			min = (float)in2.size();
+			return in2.size();
 		}			
 		else
 		{
-			max = (float)in2.size();
-			min = (float)in1.size();
+			return in1.size();
 		}
 	}
 
 	bool check_size(string in1, string in2)
 	{
-		if (in1.size() >= (in2.size() * 2))
-		{
-			return true;
-		}
-		if (in2.size() >= (in1.size() * 2))
+		if ((in1.size() >= (in2.size() * 2)) || (in2.size() >= (in1.size() * 2)))
 		{
 			return true;
 		}
 		return false;
 	}
 
-	float lengthCharacter(string in1, string in2)
+	unsigned long long lengthCharacter(string in1, string in2)
 	{
-		float max = 0;
-		float min = 0;
-		float ret = 0;
-		float value;
+		unsigned long long min = 0;
+		unsigned long long ret = 0;
+		unsigned long long gap = 0;
+
 
 		if (check_size(in1, in2)) return 0;
 
-		getMinMax(in1, in2, max, min);
+		min = getMin(in1, in2);
+		if (min == in1.size())
+			gap = in2.size() - in1.size();
+		else
+			gap = in1.size() - in2.size();
 
-		ret = (60 - ((max - min) * 60 / min));
+		ret = (60 - (gap * 60 / min));
 
-		return (int)ret;
+		return ret;
 	}
 };
